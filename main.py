26bb73ca7e98f4c1860d605ef5c6200806c11928
Michal Prokeš <michal.prokes@avast.com>
2021-02-23 19:34:55 +0200

    Smol updates

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
         members = await member.guild.fetch_members().flatten()
         for nomember in members:
-            if member.name == "Prokmi" and "Wo" in nomember.name:
+            if member.name == "Prokmi" and nomember.name == "Wolo":
                 if not nomember.dm_channel:
                     await nomember.create_dm()
                 await nomember.dm_channel.send(f"Prokmi se připojil na voice! "
                                                f"Přijď za ním, až se ti to bude hodit. :slight_smile:")
-            await member.guild.system_channel.send(f"{nomember.name} {nomember.id}")
 
 client.run(TOKEN)
