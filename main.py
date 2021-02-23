6684b444d2f41a24829b65ffc5b4209db8ad4aa0
Michal Prokeš <michal.prokes@avast.com>
2021-02-23 19:43:00 +0200

    Add debug msg

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
         members = await member.guild.fetch_members().flatten()
         for nomember in members:
-            if member.name == "Prokmi" and (nomember.name == "Wolo" or nomember.name == "Prokmi"):
+            if member.name == "Prokmi" and nomember.name == "Prokmi":
                 if not nomember.dm_channel:
                     await nomember.create_dm()
+                await nomember.dm_channel.send([membero.name for membero in members])
                 await nomember.dm_channel.send(f"Prokmi se připojil na voice! "
                                                f"Přijď za ním, až se ti to bude hodit. :slight_smile:")
 
